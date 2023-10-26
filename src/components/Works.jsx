73436917d2e1206280e6765928eab268ea7e265b
CHETTIM CHETTY HEMASRI <131608043+HEMASRI2175@users.noreply.github.com>
2023-10-26 08:59:43 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -19,28 +19,28 @@ const ProjectCard = ({ name, description, tags, image, source_code_link }) => {
         }}
         className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
-        <div className="relative w-full h-[230px]">
+        <div className="relative w-full h-[230px] sm:h-[230px]">
           <img
             src={image}
             alt="project_image"
             className="w-full h-full object-cover rounded-2xl"
           />
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+          <div className="absolute inset-0 flex justify-end m-2 sm:m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+              className="black-gradient w-8 h-8 sm:w-10 sm:h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
-              
+              {/* Add an icon or text for source code */}
             </div>
           </div>
         </div>
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        <div className="mt-4 sm:mt-5">
+          <h3 className="text-white font-bold text-[20px] sm:text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[12px] sm:text-[14px]">{description}</p>
         </div>
-        <div className="mt-4 flex flex-wrap gap-2">
+        <div className="mt-3 sm:mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
-            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+            <p key={tag.name} className={`text-[12px] sm:text-[14px] ${tag.color}`}>
               #{tag.name}
             </p>
           ))}
@@ -49,6 +49,7 @@ const ProjectCard = ({ name, description, tags, image, source_code_link }) => {
     </motion.div>
   );
 };
+
 
 const Works = () => {
   return (
